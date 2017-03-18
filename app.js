b939ddc642da59f4a4309555f8b8e676b648a284
Achmad Fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2017-03-18 18:42:26 +0700

    Create new modul to view users

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(expressValidator());
 app.use(methodOverride(function(req,res){
   if(req.body && typeof req.body == 'object' && '_method' in req.body){
     var method = req.body._mehthod;
-    detele req.body._method;
+    delete req.body._method;
     return method;
   }
 }));
